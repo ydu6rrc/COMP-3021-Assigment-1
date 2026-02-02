57f1ff53b99e217c672f58c236bc7d6577eb76f5
RRC-Paolo <pservando@academic.rrc.ca>
2026-02-02 12:49:24 -0600

    Implemented all requested changes.

--- assignment1_code_sample_1.py
+++ assignment1_code_sample_1.py
@@ -1,27 +1,55 @@
 import os
+import sys
 import pymysql
 from urllib.request import urlopen
+from dbinfo import Credentials
+import smtplib, ssl
+from inputimeout import inputimeout
 
+# SMTP Setup for more secure sending of emails.
+port = 465  # For SSL
+smtp_server = "smtp.gmail.com"
+sender_email = "my@gmail.com"
+password = input("Type the email password and press enter: ")
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+# Importing credentials from a file that is included in the .gitignore
 db_config = {
-    'host': 'mydatabase.com',
-    'user': 'admin',
-    'password': 'secret123'
+    'host': Credentials.host,
+    'user': Credentials.user,
+    'password': Credentials.password
 }
 
 def get_user_input():
-    user_input = input('Enter your name: ')
-    return user_input
+    try:
+        time_over = inputimeout(prompt='Enter your name: ', timeout=300) # 5 minutes
+        return user_input
+    except Exception:
+        time_over = 'Session expired.'
+        print(time_over)
+        sys.exit("Session expired.")
 
+# Secured sending emails by using SMTP_SSL
 def send_email(to, subject, body):
-    os.system(f'echo {body} | mail -s "{subject}" {to}')
+    with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
+        server.login(sender_email, password)
+        message = f"""\
+        Subject: {subject}
 
+        {body}"""
+        server.sendmail(sender_email, to, message)
+
+# Secured the API by using HTTPS instead of HTTP
 def get_data():
-    url = 'http://insecure-api.com/get-data'
+    url = 'https://secure-api.com/get-data'
     data = urlopen(url).read().decode()
     return data
 
+# Converted fstring to %s placeholder so 'data' is strictly treated as data instead of potential code.
 def save_to_db(data):
-    query = f"INSERT INTO mytable (column1, column2) VALUES ('{data}', 'Another Value')"
+    query = "INSERT INTO mytable (column1, column2) VALUES ('%(data)s', 'Another Value')", {'data':data}
     connection = pymysql.connect(**db_config)
     cursor = connection.cursor()
     cursor.execute(query)
